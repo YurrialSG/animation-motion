35106b2282dd6dce82c6f2515f54c2ff40c9d7f0
Yuri Silveira <yuriinternacional86@gmail.com>
2020-11-09 18:31:24 -0300

    test audio

---
+++ main.js
@@ -0,0 +1,35 @@
+import AudioMotionAnalyzer from './audiomotion-analyzer.js';
+
+try {
+    var audioMotion = new AudioMotionAnalyzer(
+        document.getElementById('animation-audio'),
+        {
+            source: document.getElementById('audio-sound'),
+            // set your preferred options here
+        }
+    );
+    const options = {
+        bgColor: '#FFFF', // background color (optional) - defaults to '#111'
+        colorStops: [       // list your gradient colors in this array (at least 2 entries are required)
+            '#12652D',                      // colors may be defined in any valid CSS format
+            { pos: 1, color: '#12652D' }, // use an object to adjust the position (0 to 1) of a color
+            'hsl( 120, 100%, 50% )'     // colors may be defined in any valid CSS format
+        ]
+    }
+    audioMotion.registerGradient('my-grad', options);
+    audioMotion.setOptions({
+        fftSize: 8192, //Frequência
+        gradient: 'my-grad', //Nome gradiente adicionado anteriormente
+        maxFreq: 22000, //Max Frequência
+        minFreq: 250, //Min Frequência
+        mode: 5, //Desenho linhas de animação - 0 até 10
+        radial: false, //Modo em circulo
+        reflexAlpha: 1,
+        reflexBright: 1,
+        showBgColor: true, //Exibir cor de fundo
+        showScale: false, //Exibir escala a baixo
+    });
+}
+catch (err) {
+    console.log(err)
+}
